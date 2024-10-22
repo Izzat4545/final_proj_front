0302f3df87145adfce3241ae2285c9126358cdb5
Izzatbek <izzatsultanov77@gmail.com>
2024-10-22 11:09:20 +0500

    Fix url reaction of the events

--- src/pages/events/Events.vue
+++ src/pages/events/Events.vue
@@ -36,9 +36,7 @@ const handleSelectChange = (event: Event) => {
   <div class="container mx-auto">
     <div class="flex justify-between">
       <div class="text-2xl mb-3 font-bold">Events</div>
-      <label for="createModal" class="text-md btn btn-success mb-3 text-white"
-        >Add event</label
-      >
+      <label for="createModal" class="text-md btn btn-success mb-3 text-white">Add event</label>
     </div>
     <!-- Events -->
     <div class="flex justify-center">
@@ -46,10 +44,7 @@ const handleSelectChange = (event: Event) => {
       <div v-if="getError" class="p-4 mt-5 text-red-800 bg-red-100 rounded-lg">
         {{ getError }}
       </div>
-      <div
-        v-if="data.length < 1 && !getError"
-        class="p-4 mt-5 text-green-800 bg-green-100 rounded-lg"
-      >
+      <div v-if="data.length < 1 && !getError" class="p-4 mt-5 text-green-800 bg-green-100 rounded-lg">
         No events has been added
       </div>
     </div>
@@ -63,52 +58,33 @@ const handleSelectChange = (event: Event) => {
       <div class="flex justify-between items-center">
         <div class="text-2xl my-3 font-bold">Gifts</div>
         <div class="flex items-center gap-2">
-          <select
-            @change="handleSelectChange"
-            :class="route.params.id ? 'select-bordered' : 'select-disabled'"
-            class="select select-md w-full max-w-xs"
-          >
+          <select @change="handleSelectChange" :class="route.params.id ? 'select-bordered' : 'select-disabled'"
+            class="select select-md w-full max-w-xs">
             <option>Show as: list</option>
             <option selected>Show as: cart</option>
           </select>
-          <label
-            for="createGiftModal"
-            :class="route.params.id ? 'btn-success' : 'btn-disabled'"
-            class="text-md btn btn-md btn-success text-white"
-            >Add Gift</label
-          >
+          <label for="createGiftModal" :class="route.params.id ? 'btn-success' : 'btn-disabled'"
+            class="text-md btn btn-md btn-success text-white">Add Gift</label>
         </div>
       </div>
       <div class="flex justify-center">
-        <span
-          v-if="giftLoading"
-          class="loading loading-infinity loading-lg"
-        ></span>
-        <div
-          v-if="giftGetError"
-          class="p-4 mt-5 text-red-800 bg-red-100 rounded-lg"
-        >
+        <span v-if="giftLoading" class="loading loading-infinity loading-lg"></span>
+        <div v-if="giftGetError" class="p-4 mt-5 text-red-800 bg-red-100 rounded-lg">
           {{ giftGetError }}
         </div>
-        <div
-          v-if="!route.params.id && !giftGetError"
-          class="p-4 mt-5 text-green-800 bg-green-100 rounded-lg"
-        >
+        <div v-if="!route.params.id && !giftGetError" class="p-4 mt-5 text-green-800 bg-green-100 rounded-lg">
           Select event
         </div>
-        <div
-          class="p-4 mt-5 text-green-800 bg-green-100 rounded-lg"
-          v-if="
-            route.params.id &&
-            !giftGetError &&
-            !giftLoading &&
-            giftData.gifts.length < 1
-          "
-        >
+        <div class="p-4 mt-5 text-green-800 bg-green-100 rounded-lg" v-if="
+          route.params.id &&
+          !giftGetError &&
+          !giftLoading &&
+          giftData.gifts.length < 1
+        ">
           There is no gift in this event
         </div>
       </div>
-      <div v-if="!giftGetError && !giftLoading && giftData.gifts.length > 0">
+      <div v-if="route.params.id && !giftGetError && !giftLoading && giftData.gifts.length > 0">
         <GiftCard v-if="!isList" :gifts="giftData" :is-public="false" />
         <GiftList v-if="isList" :gifts="giftData" :is-public="false" />
       </div>
