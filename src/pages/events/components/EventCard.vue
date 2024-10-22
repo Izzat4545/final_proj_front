b68c1d535c4b847283af472950b8ed77e214ad5f
Izzatbek Sultonov <Izzatsultanov77@gmail.com>
2024-10-22 15:49:25 +0500

    fixed flickering issue on cart loading

--- src/pages/events/components/EventCard.vue
+++ src/pages/events/components/EventCard.vue
@@ -26,9 +26,11 @@ const handleDelete = async () => {
 
 const getGiftByParams = async () => {
   try {
+    let currentId = route.params.id.toString();
     await router.push(`/events/${props.event.id}`);
-    if (route.params.id) {
+    if (route.params.id !== currentId) {
       await useGiftsStore().getGifsByEventId(route.params.id.toString());
+      currentId = route.params.id.toString();
     }
   } catch (error) {
     throw error as Error;
@@ -51,20 +53,37 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div @click="getGiftByParams" :class="route.params.id === event.id && 'border-success'"
-    class="card cursor-pointer card-compact bg-base-100 border mb-3 shadow-md p-3">
+  <div
+    @click="getGiftByParams"
+    :class="route.params.id === event.id && 'border-success'"
+    class="card cursor-pointer card-compact bg-base-100 border mb-3 shadow-md p-3"
+  >
     <figure>
-      <img crossorigin="anonymous" class="h-[130px] object-cover" :src="event.image" alt="Event Image" />
+      <img
+        crossorigin="anonymous"
+        class="h-[130px] object-cover"
+        :src="event.image"
+        alt="Event Image"
+      />
     </figure>
     <div class="card-body">
       <h2 class="card-title text-[16px]">{{ shortenText(event.title, 15) }}</h2>
       <p>Event Date: {{ event.date }}</p>
-      <div @click.stop class="flex gap-2 absolute top-0 right-2 transition-all flex-col mt-3">
-        <label :for="event.id" class="btn rounded-full btn-ghost btn-circle btn-sm">
+      <div
+        @click.stop
+        class="flex gap-2 absolute top-0 right-2 transition-all flex-col mt-3"
+      >
+        <label
+          :for="event.id"
+          class="btn rounded-full btn-ghost btn-circle btn-sm"
+        >
           <img src="../../../assets/edit.svg" alt="Edit" />
         </label>
 
-        <button @click="handleDelete" class="btn rounded-full btn-ghost btn-circle btn-sm">
+        <button
+          @click="handleDelete"
+          class="btn rounded-full btn-ghost btn-circle btn-sm"
+        >
           <img src="../../../assets/delete.svg" alt="Delete" />
         </button>
         <!-- share -->
