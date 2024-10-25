7254b88b8409146f0b5e533720ce90126fe62e57
Izzatbek Sultonov <Izzatsultanov77@gmail.com>
2024-10-25 13:20:19 +0500

    btn name fix

--- src/globalComponents/carouselComponent/PrevButton.vue
+++ src/globalComponents/carouselComponent/PrevButton.vue
@@ -1,12 +1,14 @@
 <template>
   <button
-    name="prev-btn"
+    aria-label="Next"
     class="flex items-center justify-center w-12 h-12 border-2 border-gray-300 group rounded-full bg-white transition duration-300 ease-in-out hover:bg-red-500 hover:border-red-500"
     type="button"
   >
     <svg
       class="w-5 h-5 fill-gray-800 transition duration-300 ease-in-out group-hover:fill-white"
       viewBox="0 0 532 532"
+      aria-hidden="true"
+      focusable="false"
     >
       <path
         d="M355.66 11.354c13.793-13.805 36.208-13.805 50.001 0 13.785 13.804 13.785 36.238 0 50.034L201.22 266l204.442 204.61c13.785 13.805 13.785 36.239 0 50.044-13.793 13.796-36.208 13.796-50.002 0a5994246.277 5994246.277 0 0 0-229.332-229.454 35.065 35.065 0 0 1-10.326-25.126c0-9.2 3.393-18.26 10.326-25.2C172.192 194.973 332.731 34.31 355.66 11.354Z"
