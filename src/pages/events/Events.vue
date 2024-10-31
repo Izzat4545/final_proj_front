6f6acd6c318b947f6305bc8e429176a07a65c0c2
Izzatbek <izzatsultanov77@gmail.com>
2024-10-31 18:01:42 +0500

    minor bug fix

--- src/pages/events/Events.vue
+++ src/pages/events/Events.vue
@@ -43,9 +43,7 @@ const handleSelectChange = (event: Event) => {
   <div class="container mx-auto">
     <div class="flex justify-between">
       <div class="text-2xl mb-3 font-bold">Events</div>
-      <label for="createModal" class="text-md btn btn-success mb-3 text-white"
-        >Add event</label
-      >
+      <label for="createModal" class="text-md btn btn-success mb-3 text-white">Add event</label>
     </div>
     <!-- Events -->
     <div class="flex justify-center">
@@ -53,19 +51,12 @@ const handleSelectChange = (event: Event) => {
       <div v-if="getError" class="p-4 mt-5 text-red-800 bg-red-100 rounded-lg">
         {{ getError }}
       </div>
-      <div
-        v-if="data.length < 1 && !getError && !loading"
-        class="p-4 mt-5 text-green-800 bg-green-100 rounded-lg"
-      >
+      <div v-if="data.length < 1 && !getError && !loading" class="p-4 mt-5 text-green-800 bg-green-100 rounded-lg">
         No events has been added
       </div>
     </div>
-    <Carousel
-      :autoplay="false"
-      :loop="false"
-      :show-controls="data.length > 5"
-      v-if="!loading && data.length > 0 && !getError"
-    >
+    <Carousel :autoplay="false" :loop="false" :show-controls="data.length > 5"
+      v-if="!loading && data.length > 0 && !getError">
       <div v-for="event in data" class="flex shrink-0 mx-3">
         <EventCard :event="event" />
       </div>
@@ -76,92 +67,50 @@ const handleSelectChange = (event: Event) => {
         <div class="text-2xl my-3 font-bold">Gifts</div>
         <div class="flex items-center gap-2">
           <GiftCategoryFilter default-route="/events/" />
-          <select
-            @change="handleSelectChange"
-            :class="route.params.id ? 'select-bordered' : 'select-disabled'"
-            class="select select-md w-full max-w-xs"
-          >
+          <select @change="handleSelectChange" :class="route.params.id ? 'select-bordered' : 'select-disabled'"
+            class="select select-md w-full max-w-xs">
             <option>{{ SHOW_AS_LIST }}</option>
             <option selected>{{ SHOW_AS_CART }}</option>
           </select>
-          <label
-            for="createGiftModal"
-            :class="route.params.id ? 'btn-success' : 'btn-disabled'"
-            class="text-md btn btn-md btn-success text-white"
-            >Add Gift</label
-          >
+          <label for="createGiftModal" :class="route.params.id ? 'btn-success' : 'btn-disabled'"
+            class="text-md btn btn-md btn-success text-white">Add Gift</label>
         </div>
       </div>
       <div class="flex justify-center">
-        <span
-          v-if="giftLoading"
-          class="loading loading-infinity loading-lg"
-        ></span>
-        <div
-          v-if="giftGetError"
-          class="p-4 mt-5 text-red-800 bg-red-100 rounded-lg"
-        >
+        <span v-if="giftLoading" class="loading loading-infinity loading-lg"></span>
+        <div v-if="giftGetError" class="p-4 mt-5 text-red-800 bg-red-100 rounded-lg">
           {{ giftGetError }}
         </div>
-        <div
-          v-if="!route.params.id && !giftGetError"
-          class="p-4 mt-5 text-green-800 bg-green-100 rounded-lg"
-        >
+        <div v-if="!route.params.id && !giftGetError" class="p-4 mt-5 text-green-800 bg-green-100 rounded-lg">
           Select event
         </div>
-        <div
-          class="p-4 mt-5 text-green-800 bg-green-100 rounded-lg"
-          v-if="
-            route.params.id &&
-            !giftGetError &&
-            !giftLoading &&
-            giftData.data.length < 1
-          "
-        >
-          There is no gift in this event
-        </div>
-      </div>
-      <div
-        v-if="
+        <div class="p-4 mt-5 text-green-800 bg-green-100 rounded-lg" v-if="
           route.params.id &&
           !giftGetError &&
           !giftLoading &&
-          giftData.data.length > 0
-        "
-      >
+          giftData.data.length < 1
+        ">
+          There is no gift in this event
+        </div>
+      </div>
+      <div v-if="
+        route.params.id &&
+        !giftGetError &&
+        !giftLoading &&
+        giftData.data.length > 0
+      ">
         <div
-          class="grid mx-auto max-sm:max-w-[300px] grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 my-3 justify-items-center"
-        >
-          <GiftCard
-            v-for="gift in giftData.data"
-            v-if="!isList"
-            :gift="gift"
-            :is-public="false"
-          />
+          class="grid mx-auto max-sm:max-w-[300px] grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 my-3 justify-items-center">
+          <GiftCard v-for="gift in giftData.data" v-if="!isList" :gift="gift" :is-public="false" />
         </div>
         <div class="flex-col hidden sm:flex justify-center my-3 gap-5">
-          <GiftList
-            v-for="gift in giftData.data"
-            v-if="isList"
-            :gift="gift"
-            :is-public="false"
-          />
+          <GiftList v-for="gift in giftData.data" v-if="isList" :gift="gift" :is-public="false" />
         </div>
-        <div
-          class="grid mx-auto max-sm:max-w-[300px] grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 my-3 justify-items-center"
-        >
-          <GiftCard
-            v-for="gift in giftData.data"
-            v-if="isList"
-            :key="gift.id"
-            :gift="gift"
-            :is-public="false"
-          />
+        <div :class="isList && 'grid sm:hidden'"
+          class="mx-auto max-sm:max-w-[300px] grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 my-3 justify-items-center">
+          <GiftCard v-for="gift in giftData.data" v-if="isList" :key="gift.id" :gift="gift" :is-public="false" />
         </div>
-        <div
-          v-if="giftData.meta.totalGifts > PaginationConfig.PAGE_LIMIT"
-          class="flex mb-3 justify-center"
-        >
+        <div v-if="giftData.meta.totalGifts > PaginationConfig.PAGE_LIMIT" class="flex mb-3 justify-center">
           <Pagination />
         </div>
       </div>
